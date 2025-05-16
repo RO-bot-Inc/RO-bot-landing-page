e07fba59c39d81e9cacc67ebe625c7e77d315bad
dsonders <157245742+dsonders@users.noreply.github.com>
2025-05-16 23:01:56 +0000

    Restored to '8e0b6347aa767f0dba94dd01f233aa1aef8d3318'
    Replit-Restored-To: 8e0b6347aa767f0dba94dd01f233aa1aef8d3318

--- public/script.js
+++ public/script.js
@@ -1,23 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const waitlistLinks = document.querySelectorAll('a[href="#waitlist-form"]');
-  const arrowImage = document.querySelector('.scroll-spin');
-  let scrollTimeout;
-
-  let isScrolling = false;
-
-  window.addEventListener('scroll', () => {
-    if (!isScrolling) {
-      arrowImage.classList.add('spinning');
-      isScrolling = true;
-    }
-    
-    clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(() => {
-      arrowImage.classList.remove('spinning');
-      isScrolling = false;
-    }, 150);
-  });
   
   waitlistLinks.forEach(link => {
     link.addEventListener('click', (e) => {
